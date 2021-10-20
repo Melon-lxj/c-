e362333a203892377b9a7d3a4adec88457911c03
Melon-lxj <lxjmelon@163.com>
2021-10-20 09:22:42 +0800

    1

---
+++ test10-19-2021-三/test10-19-2021-三/源.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int answer(int a, int b);
+{
+	int answer = 0;
+		int answer = a + b;
+		return answer;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", a, b);
+	int sum = answer
+		printf("%d", sum);
+	return 0;
+}
