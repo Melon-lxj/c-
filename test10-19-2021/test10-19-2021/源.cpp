a0b4b072407876a5e11a6824a5368523b96e0095
Melon-lxj <lxjmelon@163.com>
2021-10-20 09:21:05 +0800

    1

---
+++ test10-19-2021/test10-19-2021/源.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>;
+int main()
+{
+	int input = 0;
+	printf("chose 1 or 2");
+	scanf("%d", &input);
+	if (input ==  1)
+		printf("you have chose 1\n");
+	else
+		printf("you have chose 2");
+	
+			return 0;
+
+
+}
