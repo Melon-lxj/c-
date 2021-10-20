cca2ff28aaf471a28e182827513c83905ef7f7d2
Melon-lxj <lxjmelon@163.com>
2021-10-20 09:22:25 +0800

    1

---
+++ test 10-19-2021 -二/test 10-19-2021 -二/源.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int line = 0;
+	while (line < 1000)
+	{
+
+
+		printf("一起来数羊：%d\n", line);
+		line++;
+	}
+	if (line = 1000)
+	{
+		printf("睡着了");
+	}
+	return 0;
+			
+
+
+
+
+
+
+}
