f02c287d2e57c26a46623c8d429867e314fe018f
Melon-lxj <lxjmelon@163.com>
2021-10-24 23:15:01 +0800

    3个循环

---
+++ test-2021-10.20 -二/test-2021-10.20 -二/源.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int n = 0; 
+//	do
+//	{
+//		
+//			printf("%d\n", n);
+//		n++;
+//
+//
+//	} while (n < 100);
+//	return 0;*/
+//}do while Ñ­»·---
+ 
+//int main()
+//{
+//	int i = 1;
+//	while (i < 10) 
+//	{
+//
+//
+//		printf("%d    ", i);
+//		i++ ;
+//	}
+//	return 0;
+//}while Ñ­»·-----
+ 
+//int main()
+//{
+//	
+//	for (int i =0; i < 101; i++)
+//	{
+//		printf("%d  ", i);
+//	}
+//	return 0;
+//
+//}for Ñ­»·-----
