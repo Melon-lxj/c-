130e0bb17ea8224eca19b8fa13b77e63643021a2
Melon-lxj <lxjmelon@163.com>
2021-10-24 23:15:22 +0800

    三个整数按大小排列

---
+++ test-2021-10-24/test-2021-10-24/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	scanf("%d%d%d",&a, &b, &c);
+	if (a < b)
+	{
+		int min = a;
+			a = b;
+		b = min;
+	}
+	if (a < c)
+	{
+		int min = a;
+			a = c;
+		c = min;
+	}
+	if (b < c)
+	{
+		int min = b;
+			b = c;
+			c = min;
+	}
+	printf("%d %d  %d\n",a,b,c);
+	return 0;
+}
