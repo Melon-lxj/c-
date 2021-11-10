8b8d1cac103251518702163708695d36337c4bdd
Melon-lxj <lxjmelon@163.com>
2021-11-10 18:25:20 +0800

    2的n次幂

---
+++ test-2021-11-10/test-2021-11-10/源.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n = 0;
+	int a = 0;
+	int b = 0;
+	b = 1;
+	cin >> n;
+	for (a=1; a <=n; a++)
+	{
+		b *= 2;
+
+		}
+	cout << b;
+	return 0;
+}
